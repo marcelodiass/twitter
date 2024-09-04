172695c70c660ad5bc42ef1f8f5339931f767e79
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-09-04 00:03:41 -0300

    feat: HomePage component structure

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+	return (
+		<Grid container xs={12} className="px-5 lg:px-36 justify-between">
+			<Grid item xs={0} lg={2.5} className="hidden lg:block w-full relative border">
+				<p className="text-center">
+                    Left Part
+                </p>
+			</Grid>
+			<Grid item xs={12} lg={6} className="hidden lg:block w-full relative border">
+				<p className="text-center">
+                    Middle Part
+                </p>
+			</Grid>
+            <Grid item xs={0} lg={3} className="hidden lg:block w-full relative border">
+				<p className="text-center">
+                    Right Part
+                </p>
+			</Grid>
+		</Grid>
+	);
+};
 
-export default HomePage
+export default HomePage;
